83ad3920d49492cc85b712ac33ad7661518372ae
deadinsidehlp <33992861+deadinsidehlp@users.noreply.github.com>
2018-03-26 21:35:25 -0300

    Update xarutubot.js

--- xarutubot.js
+++ xarutubot.js
@@ -95,7 +95,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/javascripto/basicBot-v2/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/deadinsidehlp/meu-bot/master/langIndex.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
